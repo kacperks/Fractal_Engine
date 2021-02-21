0c237bbd27ac61307e7377a35d69ec4c08e01000
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-02-21 18:33:14 +0100

    Dodaj pliki projektów.

---
+++ src/App.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "Engine/Engine.h"
+
+int main(void)
+{
+   
+}
---
+++ src/pch.h
@@ -0,0 +1,18 @@
+#pragma once
+
+//Basic
+#include <set>
+#include <map>
+#include <vector>
+#include <string>
+#include <memory>
+#include <cassert>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+//Open GL 
+#include <GLFW/glfw3.h>
+#include <GL/glew.h>
