794e9c2adb36ab586b3fef79ea3ddf2108b1925b
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-03-02 13:32:37 +0100

    GL libs for windows and small update

--- src/Editor/editor.cpp
+++ src/Editor/editor.cpp
@@ -1,5 +1,12 @@
 #include "../fractal.hpp"
 
-int main(){
-    
-}
+int main(int argc, char** argv) {
+
+	fr::Core.Initialize();
+
+	while (fr::Core.OnRunApp()) {
+		fr::Core.Update();
+	}
+
+	return fr::EXIT_PROG_SUCCESS;
+} // not finished yet
