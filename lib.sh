62d6cd496f5d3057d921eb16b86910c1226f6e2a
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-03-04 12:35:15 +0100

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -2,3 +2,6 @@ sudo apt install libglm-dev
 sudo apt install libglew-dev
 sudo apt install libsfml-dev
 sudo apt install libglfw3-dev
+sudo apt-get install libassimp-dev assimp-utils
+sudo apt-get install libxmu-dev libxi-dev
+echo "Instlled! now you can bulid Fractal Engine!"
