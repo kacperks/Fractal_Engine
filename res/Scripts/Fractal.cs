ae784cb38b53af11b294e343e20e8c6750f710d5
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-03-20 13:24:58 +0100

    Update Fractal.cs

--- res/Scripts/Fractal.cs
+++ res/Scripts/Fractal.cs
@@ -2,40 +2,48 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
-[StructLayout(LayoutKind.Sequential)]
-public struct Vector3 {
+namespace Fractal
+{
 
-    public Single X;
-    public Single Y;
-    public Single Z;
-
-    public Vector3(Single x, Single y, float z) {
-        X = x;
-        Y = y;
-        Z = z;
-    }
-
-    public Vector3(Single v) {
-        X = v;
-        Y = v;
-        Z = v;
-    }
-}
-
-[StructLayout(LayoutKind.Sequential)]
-public struct Transform {
-    private int Entity;
-    public Vector3 Position;
-    public Vector3 Rotation;
-    public Vector3 Scale;
-    public int EntityID { get { return Entity; } }
-}
-
-
-
-public class FractalScript
-    static void Main(string[] args)
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Vector3
     {
-        Console.WriteLine(args.Length);
-    }   
+
+        public Single X;
+        public Single Y;
+        public Single Z;
+
+        public Vector3(Single x, Single y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vector3(Single v)
+        {
+            X = v;
+            Y = v;
+            Z = v;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Transform
+    {
+        private int Entity;
+        public Vector3 Position;
+        public Vector3 Rotation;
+        public Vector3 Scale;
+        public int EntityID { get { return Entity; } }
+    }
+
+    public class FractalScript
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+        }
+    }
+
 }
