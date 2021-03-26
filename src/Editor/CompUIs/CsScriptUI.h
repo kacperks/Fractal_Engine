f6182432a6ef6391d613fd095c5d198a0d0d9da0
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-03-26 22:56:26 +0100

    DLL
    Co-Authored-By: lubek <70327203+lubek-dc@users.noreply.github.com>

--- src/Editor/CompUIs/CsScriptUI.h
+++ src/Editor/CompUIs/CsScriptUI.h
@@ -1,47 +1,39 @@
 #pragma once
 
 #include "CompUI.h"
-#include "../../ECS/Components/EntityName.h"
+#include "Resource/Resource.h"
+#include "ECS/Components/CsScript.h"
 
-class CsScriptUI : public CompUI {
-
-public:
-	NameTagUI(): buffer(), eName(nullptr) {}
-
-	void Init(ECS::EntityID entity) override {
-		isVisible = false;
-		if (ECS::Manager.HasComponent<CsScript>(entity)) {
-			script = &ECS::Manager.GetComponent<CsScript>(entity);
-			isVisible = true;
-		}
-	}
-
-	void Show() override {	
-
-		if (ImGui::CollapsingHeader("CS Scripts")) {
-			ImGui::SetCurrentFont(ImGui::GetIO().Fonts->Fonts[1]);
-			ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, FRAME_PADDING);
-			ImGui::PushStyleColor(ImGuiCol_FrameBg, DARK);
-
-			ImGui::BeginChildFrame(ImGui::GetID("scriptsframe"), ImVec2(0, 35));
-			ImGui::PopStyleVar();
-			ImGui::PopStyleColor();
-			{
-				ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(3, 3));
-				strcpy_s(buffer, DLLpath->Value.c_str());
-				if(ImGui::InputText("FileName", buffer, IM_ARRAYSIZE(buffer))) {
-					DLLpath->Value = std::string(buffer);
-				}
-				ImGui::PopStyleVar();
+class CsCompUI : CompUI {
+	public:
+		std::string ValueDLL;
+		std::string ValueClass;
+		void Init(ECS::EntityID entity) override {
+			isVisible = false;
+			if (ECS::Manager.HasComponent<CsScript>(entity)) {
+				script = &ECS::Manager.GetComponent<CsScript>(entity);
+				isVisible = true;
 			}
-			ImGui::EndChildFrame();
-			ImGui::SetCurrentFont(ImGui::GetIO().Fonts->Fonts[0]);
 		}
-	}
+		void Show() override {
+			if (ImGui::CollapsingHeader("C# Script")) {
+				ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, FRAME_PADDING);
+				ImGui::PushStyleColor(ImGuiCol_FrameBg, DARK);
 
+				ImGui::BeginChildFrame(ImGui::GetID("csframe"), ImVec2(0, 75));
+				ImGui::PopStyleVar();
+				ImGui::PopStyleColor();
+				{
+					//if (ImGui::InputText("Class Name", buffer2, IM_ARRAYSIZE(buffer2))) {
+					//	ValueClass = std::string(buffer2);
+					//}
+					if (ImGui::InputText("DLL path", buffer, IM_ARRAYSIZE(buffer))) {
+						CsScript::AssemblyPath = std::string(buffer);
+					}
+				}
+			}
+		}
 private:
-	CsScript script;
 	char buffer[20];
-	std::string ClassName;
-	std::string DLLpath;
+	CsScript* script;
 };
