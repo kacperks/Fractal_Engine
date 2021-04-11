33bdb93667dd61d84564a6292120faf9423e6dbe
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-04-11 13:59:04 +0200

    Update

--- lib/lib.sh
+++ lib/lib.sh
@@ -4,4 +4,7 @@ sudo apt install libsfml-dev
 sudo apt install libglfw3-dev
 sudo apt-get install libassimp-dev assimp-utils
 sudo apt-get install libxmu-dev libxi-dev
+sudo apt install mono-complete
+sudo apt-get install -y libtinyxml-dev
+
 echo "Instlled! now you can bulid Fractal Engine!"
