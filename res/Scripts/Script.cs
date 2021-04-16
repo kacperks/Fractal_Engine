ce38284d4f8aed4f53b28645ef141e96b85c14a5
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-04-16 12:19:30 +0200

    Resource Update v2
    Co-Authored-By: lubek <70327203+lubek-dc@users.noreply.github.com>

--- res/Scripts/Script.cs
+++ res/Scripts/Script.cs
@@ -1,9 +1,56 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
-using Fractal;
 
-public class TestScript : FractalScript
+[StructLayout(LayoutKind.Sequential)]
+public struct Vector3
+{
+
+    public Single X;
+    public Single Y;
+    public Single Z;
+
+    public Vector3(Single x, Single y, float z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public Vector3(Single v)
+    {
+        X = v;
+        Y = v;
+        Z = v;
+    }
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct Transform
+{
+    private int Entity;
+    public Vector3 Position;
+    public Vector3 Rotation;
+    public Vector3 Scale;
+    public int EntityID { get { return Entity; } }
+}
+
+public struct EntityName
+{
+    private int Entity;
+    public string Value;
+    public int EntityID { get { return Entity; } }
+}
+
+public class Orbit3DScript
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(args.Length);
+    }
+}
+
+public class TestScript : Orbit3DScript
 {
     public float Speed;
     public void Start()
