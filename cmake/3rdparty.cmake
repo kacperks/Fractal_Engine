dad9fbfd08689b86c38613b86f540fe01c9241f0
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-04-27 16:10:04 +0200

    Delete cmake directory

--- cmake/3rdparty.cmake
+++
@@ -1,5 +0,0 @@
-set(OpenGL_GL_PREFERENCE GLVND)
-find_package(OpenGL REQUIRED)
-
-include(cmake/glfw.cmake)
-include(cmake/imgui.cmake)
--- cmake/README.md
+++
@@ -1,5 +0,0 @@
-# CMAKE
-
-Cmake Bulid sooon!
-
-Now you can bulid only with Visual Studio 2019
