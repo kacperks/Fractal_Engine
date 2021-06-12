420b17dc9e8f436687ac6e611acc517aa8a8514d
Tuncay Yavuz Büyükadalı <76534892+Yavuz1234567890@users.noreply.github.com>
2021-06-12 19:36:10 +0300

    Somethink

--- Engine/src/Math/Matrix4.h
+++ Engine/src/Math/Matrix4.h
@@ -8,13 +8,12 @@ namespace fr {
         Mat4(float diagonal) :
             e0(diagonal), e1(0.0f), e2(0.0f), e3(0.0f), e4(0.0f), e5(diagonal),
             e6(0.0f), e7(0.0f), e8(0.0f), e9(0.0f), e10(diagonal),
-            e11(0.0f), e12(0.0f), e13(0.0f), e14(0.0f), e15(diagonal), created(true) {
+            e11(0.0f), e12(0.0f), e13(0.0f), e14(0.0f), e15(diagonal) {
             InitArray();
         }
         
         static Mat4& Translate(const Vec3& pos) {
             Mat4& mat = Mat4(1.0f);
-            if(!mat.created)return;
             mat.e12 = pos.x;
             mat.e13 = pos.y;
             mat.e14 = pos.z;
@@ -24,7 +23,6 @@ namespace fr {
         
         static Mat4& Scale(const Vec3& scale) {
             Mat4& mat = Mat4(1.0f);
-            if(!mat.created)return;
             mat.e0 = scale.x;
             mat.e5 = scale.y;
             mat.e10 = scale.z;
@@ -34,7 +32,6 @@ namespace fr {
         
         float e0, e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15;
         float matrix[16];
-        bool created = false;
         
         void InitArray(){
             matrix[0] = e0;
