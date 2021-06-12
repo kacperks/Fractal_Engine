a391059125fb668c46914d4e735b05ce80f1d01c
Tuncay Yavuz Büyükadalı <76534892+Yavuz1234567890@users.noreply.github.com>
2021-06-12 18:29:45 +0300

    Update Matrix4.h

--- Engine/src/Math/Matrix4.h
+++ Engine/src/Math/Matrix4.h
@@ -8,14 +8,14 @@ namespace fr {
             e0(diagonal), e1(0.0f), e2(0.0f), e3(0.0f), e4(0.0f), e5(diagonal),
             e6(0.0f), e7(0.0f), e8(0.0f), e9(0.0f), e10(diagonal),
             e11(0.0f), e12(0.0f), e13(0.0f), e14(0.0f), e15(diagonal) {
-
+            InitPtr();
         }
 
         float e0, e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15;
+        float matrix[16];
         
-        float* ToPtr(){
-            float matrix[16] = { e0, e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15 };
-            return matrix;
+        void InitPtr(){
+            matrix = { e0, e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15 };
         }
     };
 }
