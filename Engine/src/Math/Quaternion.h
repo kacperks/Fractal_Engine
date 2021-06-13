2121cb77b5a6f266a73b930645d56797a87d6620
Tuncay Yavuz Büyükadalı <76534892+Yavuz1234567890@users.noreply.github.com>
2021-06-13 11:49:17 +0300

    quat update

--- Engine/src/Math/Quaternion.h
+++ Engine/src/Math/Quaternion.h
@@ -7,14 +7,28 @@ namespace fr {
         Quaternion(): x(0.0f), y(0.0f), z(0.0f), w(0.0f) { }
         Quaternion(float x, float y, float z, float w): x(x), y(y), z(z), w(w) { }
         
-        float Length() { return (float)std::sqrt(x * x, y * y, z * z, w * w); }
-      
-        void Normalize() {
-            float l = Length();
-            x /= l;
-            y /= l;
-            z /= l;
-            w /= l;
+        Quaternion operator=(Vec3& other) {
+            x = other.x;
+            y = other.y;
+            z = other.z;
+            w = 0.0f;
+            return *this;
+        }
+        
+        Quaternion operator+(Quaternion& other) {
+            return Quaternion(x + other.x, y + other.y, z + other.z, w + other.w);   
+        }
+        
+        Quaternion operator-(Quaternion& other) {
+            return Quaternion(x - other.x, y - other.y, z - other.z, w - other.w);   
+        }
+        
+        Quaternion operator*(Quaternion& other) {
+            return Quaternion(x * other.x, y * other.y, z * other.z, w * other.w);   
+        }
+        
+        Quaternion operator/(Quaternion& other) {
+            return Quaternion(x / other.x, y / other.y, z / other.z, w / other.w);   
         }
       
         float x, y, z, w;
