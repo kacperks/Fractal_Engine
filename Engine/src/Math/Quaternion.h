e4f445a272c0ff33ed0e06e40b9746a56253b653
Tuncay Yavuz Büyükadalı <76534892+Yavuz1234567890@users.noreply.github.com>
2021-06-13 13:52:09 +0300

    quat update

--- Engine/src/Math/Quaternion.h
+++ Engine/src/Math/Quaternion.h
@@ -1,28 +1,33 @@
 #include "pch.h"
 #include "Math.h"
-#include "Vector3.h"
+#include "Vector4.h"
 
 namespace fr {
     struct Quaternion {
-        Quaternion(): x(0.0f), y(0.0f), z(0.0f), w(0.0f) { }
-        Quaternion(float x, float y, float z, float w): x(x), y(y), z(z), w(w) { }
+        Quaternion(): xyzw({ 0.0f, 0.0f, 0.0f, 0.0f }) { }
+        Quaternion(Vec4& xyzw): xyzw(xyzw) { }
+        
+        Quaternion operator=(Vec4& other) {
+            xyzw = other;
+            return *this;
+        }
         
         Quaternion operator+(Quaternion& other) {
-            return Quaternion(x + other.x, y + other.y, z + other.z, w + other.w);   
+            return Quaternion(xyzw + other.xyzw);   
         }
         
         Quaternion operator-(Quaternion& other) {
-            return Quaternion(x - other.x, y - other.y, z - other.z, w - other.w);   
+            return Quaternion(xyzw - other.xyzw);   
         }
         
         Quaternion operator*(Quaternion& other) {
-            return Quaternion(x * other.x, y * other.y, z * other.z, w * other.w);   
+            return Quaternion(xyzw * other.xyzw);   
         }
         
         Quaternion operator/(Quaternion& other) {
-            return Quaternion(x / other.x, y / other.y, z / other.z, w / other.w);   
+            return Quaternion(xyzw / other.xyzw);   
         }
-      
-        float x, y, z, w;
+        
+        Vec4 xyzw;
     };
 }
