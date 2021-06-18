51d7e51f2fb29c6037032696512a40af361ac966
Tuncay Yavuz Büyükadalı <76534892+Yavuz1234567890@users.noreply.github.com>
2021-06-18 14:17:36 +0300

    destroy function added in matrix4

--- Engine/src/Math/Matrix4.h
+++ Engine/src/Math/Matrix4.h
@@ -7,6 +7,7 @@
 #include "Vector3.h"
 #include "Vector4.h"
 
+#include <stdlib.h>
 #include <malloc.h>
 
 #define SIZE 4
@@ -94,7 +95,10 @@ namespace fr {
             mat.InitArray();
             return mat;
         }
-
+        
+        void Destroy() {
+            free(matrix);   
+        }
 
         float e0, e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15;
         float* matrix;
