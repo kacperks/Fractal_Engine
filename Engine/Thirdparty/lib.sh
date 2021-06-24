5f801e2dccc17080231f57264a47bc5457a08b66
Kacper Kostka <kacper.kostka08@gmail.com>
2021-06-24 20:22:27 +0200

    CMakeLists.txt added!

--- Engine/Thirdparty/lib.sh
+++
@@ -1,8 +0,0 @@
-sudo apt install libglm-dev
-sudo apt install libglew-dev
-sudo apt install libsfml-dev
-sudo apt install libglfw3-dev
-sudo apt-get install libassimp-dev assimp-utils
-sudo apt-get install libxmu-dev libxi-dev
-sudo apt install mono-complete
-sudo apt-get install -y libtinyxml-dev
