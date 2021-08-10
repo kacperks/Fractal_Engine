e39a28d37d20cb1683c4dc2c05fb7379239f8285
Tuncay Yavuz Büyükadalı <76534892+Yavuz1234567890@users.noreply.github.com>
2021-08-10 15:19:51 +0300

    New matrix

--- Engine/Source/Math/Matrix4.h
+++ Engine/Source/Math/Matrix4.h
@@ -4,54 +4,237 @@
 #define FR_MATRIX_4
 
 #include "Math.h"
+#include "Vector2.h"
 #include "Vector3.h"
 #include "Vector4.h"
+#include <cmath>
 
 namespace fr {
         struct Mat4 {
-            Mat4(float diagonal) :
-                e0(diagonal), e1(0.0f), e2(0.0f), e3(0.0f), e4(0.0f), e5(diagonal),
-                e6(0.0f), e7(0.0f), e8(0.0f), e9(0.0f), e10(diagonal),
-                e11(0.0f), e12(0.0f), e13(0.0f), e14(0.0f), e15(diagonal) {
-                InitArray();
-            }
-
-            static Mat4& Translate(const Vec3& pos);
-            static Mat4& Scale(Vec3& scale);
-            static Mat4& Rotate(float angle, Vec3& axis);
-            static Mat4& Ortho(float left, float right, float bottom, float top, float near1, float far1);
-
-            static Mat4& Perspective(float fov, float aspectRatio, float near1, float far1);
-            static Mat4 Mul(Mat4 t, Mat4 other);
-
-            float e0, e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15;
-            float matrix[16];
-            void InitArray() {
-                matrix[0] = e0;
-                matrix[1] = e1;
-                matrix[2] = e2;
-                matrix[3] = e3;
-                matrix[4] = e4;
-                matrix[5] = e5;
-                matrix[6] = e6;
-                matrix[7] = e7;
-                matrix[8] = e8;
-                matrix[9] = e9;
-                matrix[10] = e10;
-                matrix[11] = e11;
-                matrix[12] = e12;
-                matrix[13] = e13;
-                matrix[14] = e14;
-                matrix[15] = e15;
-            }
-
-            Mat4 operator=(Vec4 vecs[4]) {
-                e0 = vecs[0].x; e1 = vecs[0].y; e2 = vecs[0].z; e3 = vecs[0].w;
-                e4 = vecs[1].x; e5 = vecs[1].y; e6 = vecs[1].z; e7 = vecs[1].w;
-                e8 = vecs[2].x; e9 = vecs[2].y; e10 = vecs[2].z; e11 = vecs[2].w;
-                e12 = vecs[3].x; e13 = vecs[3].y; e14 = vecs[3].z; e15 = vecs[3].w;
-            }
-
+        private:
+                float matrix[4][4];
+        public:
+                Mat4() {
+                        SetZero();
+                }
+                
+                Mat4(float e00, float e01, float e02, float e03,
+                     float e10, float e11, float e12, float e13,
+                     float e20, float e21, float e22, float e23,
+                     float e30, float e31, float e32, float e33) {
+                        matrix[0][0] = e00; matrix[0][1] = e01; matrix[0][2] = e02; matrix[0][3] = e03;
+                        matrix[1][0] = e10; matrix[1][1] = e11; matrix[1][2] = e12; matrix[1][3] = e13;
+                        matrix[2][0] = e20; matrix[2][1] = e21; matrix[2][2] = e22; matrix[2][3] = e23;
+                        matrix[3][0] = e30; matrix[3][1] = e31; matrix[3][2] = e32; matrix[3][3] = e33;
+                }
+                
+                Mat4(Vec4& vec) {
+                        matrix[0][0] = vec.x; matrix[0][1] = vec.y; matrix[0][2] = vec.z; matrix[0][3] = vec.w;
+                        matrix[1][0] = vec.x; matrix[1][1] = vec.y; matrix[1][2] = vec.z; matrix[1][3] = vec.w;
+                        matrix[2][0] = vec.x; matrix[2][1] = vec.y; matrix[2][2] = vec.z; matrix[2][3] = vec.w;
+                        matrix[3][0] = vec.x; matrix[3][1] = vec.y; matrix[3][2] = vec.z; matrix[3][3] = vec.w;
+                }
+                
+                Mat4(Vec3& pos, Vec3& scale, Vec3& rot) {
+                        SetZero();
+                        
+                        SetPosition(pos);
+                        SetScale(scale);
+                        SetRotX(rot.x);
+                        SetRotY(rot.y);
+                        SetRotZ(rot.z);
+                }
+                
+                void SetZero() {
+                        matrix[0][0] = 0.0f; matrix[0][1] = 0.0f; matrix[0][2] = 0.0f; matrix[0][3] = 0.0f;
+                        matrix[1][0] = 0.0f; matrix[1][1] = 0.0f; matrix[1][2] = 0.0f; matrix[1][3] = 0.0f;
+                        matrix[2][0] = 0.0f; matrix[2][1] = 0.0f; matrix[2][2] = 0.0f; matrix[2][3] = 0.0f;
+                        matrix[3][0] = 0.0f; matrix[3][1] = 0.0f; matrix[3][2] = 0.0f; matrix[3][3] = 0.0f;
+                }
+                
+                void SetIdentity() {
+                        matrix[0][0] = 1.0f;
+                        matrix[1][1] = 1.0f;
+                        matrix[2][2] = 1.0f;
+                        matrix[3][3] = 1.0f;
+                }
+                
+                void SetPosition(Vec3& pos) {
+                        matrix[0][3] = pos.x;
+                        matrix[1][3] = pos.y;
+                        matrix[2][3] = pos.z;
+                }
+                        
+                void SetScale(Vec3& scale) {
+                        matrix[0][0] = scale.x;
+                        matrix[1][1] = scale.y;
+                        matrix[2][2] = scale.z;
+                }
+                
+                void SetRotX(float angle) {
+                        float c = std::cosf(Math::Radians(angle));
+                        float s = std::sinf(Math::Radians(angle));
+                        
+                        matrix[1][1] = c;
+                        matrix[1][2] = -s;
+                        matrix[2][1] = s;
+                        matrix[2][2] = c;
+                }
+                
+                void SetRotY(float angle) {
+                        float c = std::cosf(Math::Radians(angle));
+                        float s = std::sinf(Math::Radians(angle));
+                        
+                        matrix[0][0] = c;
+                        matrix[0][2] = s;
+                        matrix[2][0] = -s;
+                        matrix[2][2] = c;
+                }
+                
+                void SetRotZ(float angle) {
+                        float c = std::cosf(Math::Radians(angle));
+                        float s = std::sinf(Math::Radians(angle));
+                        
+                        matrix[0][0] = c;
+                        matrix[0][1] = -s;
+                        matrix[1][0] = s;
+                        matrix[1][1] = c;
+                }
+                
+                void SetOrthographic(float left, float right, float bottom, float top, float near, float far) {
+                        matrix[0][0] = 2.0f / (right - left);
+		        matrix[1][1] = 2.0f / (top - bottom);
+		        matrix[2][2] = 2.0f / (near - far);
+		        matrix[3][0] = (left + right) / (left - right);
+		        matrix[3][1] = (bottom + top) / (bottom - top);
+		        matrix[3][2] = (far + near) / (far - near);
+                }
+                
+                void SetPerspective(float fov, float aspectRatio, float near, float far) {
+                        float q = 1.0f / tan(toRadians(0.5f * fov));
+		        float a = q / aspectRatio;
+		        float b = (near + far) / (near - far);
+		        float c = (2.0f * near * far) / (near - far);
+    
+		        matrix[0][0] = a;
+		        matrix[1][1] = q;
+		        matrix[2][2] = b;
+		        matrix[2][3] = -1.0f;
+		        matrix[3][2] = c;
+                }
+                
+                static Mat4 Translate(Vec3& pos) {
+                        Mat4 mat;
+                        mat.SetPosition(pos);
+                        return mat;
+                }
+                
+                static Mat4 Scale(Vec3& s) {
+                        Mat4 mat;
+                        mat.SetScale(s);
+                        return mat;
+                }
+                
+                static Mat4 Rotate(Vec3& rot) {
+                        Mat4 mat;
+                        mat.SetRotX(rot.x);
+                        mat.SetRotY(rot.y);
+                        mat.SetRotZ(rot.z);
+                        return mat;
+                }
+                
+                float Get(int x, int y) {
+                        return matrix[x][y];
+                }
+                
+                void Set(int x, int y, float val) {
+                        matrix[x][y] = val;
+                }
+                
+                Mat4& operator*(Mat4& other) {
+                        Mat4 mat;
+                        for(int x = 0; x < 4; ++x) {
+                                for(int y = 0; y < 4; ++y) {
+                                        float m = 0.0f;
+                                        for(int i = 0; i < 4; ++i) {
+                                                m += matrix[x][i] * other.matrix[i][y];
+                                        }
+                                        mat[x][y] = m;
+                                }
+                        }
+                        return mat;
+                }
+                
+                Mat4& operator/(Mat4& other) {
+                        return Mat4(matrix[0][0] / other.matrix[0][0], matrix[0][1] / other.matrix[0][1], matrix[0][2] / other.matrix[0][2], matrix[0][3] / other.matrix[0][3],
+                                    matrix[1][0] / other.matrix[1][0], matrix[1][1] / other.matrix[1][1], matrix[1][2] / other.matrix[1][2], matrix[1][3] / other.matrix[1][3],
+                                    matrix[2][0] / other.matrix[2][0], matrix[2][1] / other.matrix[2][1], matrix[2][2] / other.matrix[2][2], matrix[2][3] / other.matrix[2][3],
+                                    matrix[3][0] / other.matrix[3][0], matrix[3][1] / other.matrix[3][1], matrix[3][2] / other.matrix[3][2], matrix[3][3] / other.matrix[3][3]);
+                }
+                
+                Mat4& operator+(Mat4& other) {
+                        return Mat4(matrix[0][0] + other.matrix[0][0], matrix[0][1] + other.matrix[0][1], matrix[0][2] + other.matrix[0][2], matrix[0][3] + other.matrix[0][3],
+                                    matrix[1][0] + other.matrix[1][0], matrix[1][1] + other.matrix[1][1], matrix[1][2] + other.matrix[1][2], matrix[1][3] + other.matrix[1][3],
+                                    matrix[2][0] + other.matrix[2][0], matrix[2][1] + other.matrix[2][1], matrix[2][2] + other.matrix[2][2], matrix[2][3] + other.matrix[2][3],
+                                    matrix[3][0] + other.matrix[3][0], matrix[3][1] + other.matrix[3][1], matrix[3][2] + other.matrix[3][2], matrix[3][3] + other.matrix[3][3]);
+                }
+                
+                Mat4& operator-(Mat4& other) {
+                        return Mat4(matrix[0][0] - other.matrix[0][0], matrix[0][1] - other.matrix[0][1], matrix[0][2] - other.matrix[0][2], matrix[0][3] - other.matrix[0][3],
+                                    matrix[1][0] - other.matrix[1][0], matrix[1][1] - other.matrix[1][1], matrix[1][2] - other.matrix[1][2], matrix[1][3] - other.matrix[1][3],
+                                    matrix[2][0] - other.matrix[2][0], matrix[2][1] - other.matrix[2][1], matrix[2][2] - other.matrix[2][2], matrix[2][3] - other.matrix[2][3],
+                                    matrix[3][0] - other.matrix[3][0], matrix[3][1] - other.matrix[3][1], matrix[3][2] - other.matrix[3][2], matrix[3][3] - other.matrix[3][3]);
+                }
+                
+                Mat4& operator*(Vec4& other) {
+                        return (*this) * Mat4(other);
+                }
+                
+                Mat4& operator/(Vec4& other) {
+                        return (*this) / Mat4(other);
+                }
+                
+                Mat4& operator+(Vec4& other) {
+                        return (*this) + Mat4(other);
+                }
+                
+                Mat4& operator-(Vec4& other) {
+                        return (*this) - Mat4(other);
+                }
+                
+                Mat4 Mul(Mat4& other) {
+                        return (*this) * other;
+                }
+                
+                Mat4 Div(Mat4& other) {
+                        return (*this) / other;
+                }
+                
+                Mat4 Add(Mat4& other) {
+                        return (*this) + other;
+                }
+                
+                Mat4 Min(Mat4& other) {
+                        return (*this) - other;
+                }
+                
+                Mat4 Mul(Vec4& other) {
+                        return (*this) * other;
+                }
+                
+                Mat4 Div(Vec4& other) {
+                        return (*this) / other;
+                }
+                
+                Mat4 Add(Vec4& other) {
+                        return (*this) + other;
+                }
+                
+                Mat4 Min(Vec4& other) {
+                        return (*this) - other;
+                }
+                
+                float* ValuePointer() { return &(matrix[0][0]); }
         };
     }
 
