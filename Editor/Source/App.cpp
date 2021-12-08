3af278ab0d8391eb53af5111b7d6bbee3a73667e
kacperks <kacper.kostka08@gmail.com>
2021-12-08 23:44:34 +0100

    E

--- Editor/Source/App.cpp
+++ Editor/Source/App.cpp
@@ -10,23 +10,22 @@
 using namespace fr;
 
 int main(int argc, char** argv) {
-	Resource.LoadModel("WTF", "Resource/Models/Nature/NatureFreePack1.obj");
-	Resource.LoadModel("Car", "Resource/Models/car.obj");
+	//Resource.LoadModel("WTF", "Resource/Models/Nature/NatureFreePack1.obj");
+	//Resource.LoadModel("Car", "Resource/Models/car.obj");
 	Resource.Initialize();
 	Core.Initialize();
-	ECS::Manager.AddEditorSystem<EditorCameraSystem>();
-	UI.Initialiaze();
+	//ECS::Manager.AddEditorSystem<EditorCameraSystem>();
+	//UI.Initialiaze();
 	Timer.Initialize();
-	Serializer.LoadScene("Resource/scene/scene.fr");
-
+	
 	while (Core.Run()) {
 		Timer.Tick();
 		Events.Poll();
 		Core.Update();
 
 		if (!Core.GameRun()) {
-			Core.SetViewport(UI.ViewSize());
-			UI.Display();
+			//Core.SetViewport(UI.ViewSize());
+			//UI.Display();
 		}
 		Core.Render();
 	}
